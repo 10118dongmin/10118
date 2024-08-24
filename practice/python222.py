8c19edf2009852e2ac276279b0e661ab908cd2f6
User me. <dmchoi921@naver.com>
2024-08-24 10:52:44 +0900

    변수 call by value, call by reference 차이

--- practice/python222.py
+++ practice/python222.py
@@ -5,8 +5,25 @@ print(a)
 a=0.5
 print(a)
 
-b = input("값을 입력하세요")
-print(b +"(이)가 입력되었습니다.")
+# 입력한 값을 변수에 대입
+# b = input("값을 입력하세요 :")
+# print(b +"(이)가 입력되었습니다.")
 
-c = int(b) +100 #형변환
+b = "100"
+c = int(b) + 100 # 형변환
 print(c)
+
+d = c
+print("c의 값은 ", c)
+print("d의 값은 ", d)
+c = 100
+print("c의 값은 ", c)
+print("d의 값은 ", d)
+
+e = [100, 96, 87, 100]
+f = e
+print(e)
+print(f)
+e.append(95)
+print(e)
+print(f)
