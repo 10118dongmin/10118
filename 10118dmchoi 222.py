5b861f78bf6370b38df05edc7b7ac1b66c1b177d
10118최동민 10118dongmin <dmchoi921@naver.com>
2024-10-12 10:22:13 +0900

    boolean(True/False) 예제

---
+++ 10118dmchoi 222.py
@@ -0,0 +1,37 @@
+# True / False
+
+print(True)
+print("0==True", 0==True)
+print("1==True", 1==True)
+print("2==True", 2==True)
+
+print("0==False", 0==False)
+print("1==False", 1==False)
+print("2==False", 2==False)
+
+
+print("=========")
+
+a = 10
+b = 10
+c = 15
+
+
+print("a==b", a==b)
+print("a!=b", a!=b)
+print("a>b", a>b)
+print("a>=b", a>=b)
+print("a<b", a<b)
+print("a<=b", a<=b)
+
+print("=========")
+print("a==c", a==c)
+print("a!=c", a!=c)
+print("a>c", a>c)
+print("a>=c", a>=c)
+print("a<c", a<c)
+print("a<=c", a<=c)
+
+print("===========")
+print(a+b > c)
+print(a == 10)
