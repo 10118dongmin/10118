bd81b01f3883d1bfb8ee9ea6b6c87bdde9760ad4
10118최동민 <dmchoi921@naver.com>
2025-02-01 10:37:20 +0900

    2/1

---
+++ 2/1.py
@@ -0,0 +1,25 @@
+d = {"key":"value", 2.3: "value", 5:1, 5:"111"}
+print(d)
+
+print(d.keys())
+print(d.values())
+
+"""
+# 자판기 프로그램을 할 것
+# 사용자모드 / 관리자모드
+# 사용자모드 - 물건 구매
+# 관리자모드 - 물건 추가, 물건 가격 변동
+inventory = {}
+#사용자 모드
+
+if not inventory :
+    print("X")
+elif inventory :
+    answer = input("선택")
+    if inventory.key == answer:
+        print("O")
+    else:
+        print("X")
+
+#관리자 모드
+c = input("입력")
